1fdd95cbc356ce89cbcb36eb4647e5848f820a71
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-07-16 22:23:55 -0500

    Able to isolate the state strings but now have to figure out how to select one from the dropDown menu for comparison

--- src/StatePop.js
+++ src/StatePop.js
@@ -3,20 +3,34 @@ import React from 'react';
 const StatePop = props => {
   const loopStates = props.results;
 
-  // function checkstatesFunc() {
-  //   console.log(123);
-  // }
+  const loopTest = loopStates.map(st => (
+    <option value={st.state} key={st.state}>
+      {st.state}
+    </option>
+  ));
+
+  // const testingSt = loopTest.map(t => {
+  //   console.log(t.props.value);
+  // });
+  const GrabSt = e => {
+    loopTest.map(t => {
+      const grSt = t.props.value;
+    });
+    // console.log(e.target.value);
+  };
 
   return (
     <div className="state-dropdown">
       <select>
-        {loopStates.map(st => (
+        {/* {loopStates.map(st => (
           <option value={st.state} key={st.state}>
             {st.state}
           </option>
-        ))}
+        ))} */}
+        {/* <LoopTest onClick={loopStates} /> */}
+        {loopTest}
       </select>
-      {/* <button onClick={checkstatesFunc}>Check State</button> */}
+      <button onClick={GrabSt}>Check State</button>
     </div>
   );
 };
