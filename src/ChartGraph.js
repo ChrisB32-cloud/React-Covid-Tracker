b1ada2704b919837da4f3b3905ea778f3d0575c5
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2020-07-28 15:02:51 -0500

    Added a second chart for mortality but have issues with the chart reading in the 0 value, when I get null for the value I set it to 0 with a conditional, I have it working right now because i have the value set to 1, but that's not correct data.

--- src/ChartGraph.js
+++ src/ChartGraph.js
@@ -31,6 +31,21 @@ const ChartGraph = props => {
     return filt.date;
   });
 
+  const desChart = dataApi.filter((mort, idx) => {
+    if (idx % 5 === 0) {
+      if (mort.death === null) {
+        return (mort.death = 1);
+      }
+      return mort.death;
+    }
+  });
+
+  const desChartData = desChart.map(mort => {
+    return mort.death;
+  });
+
+  // console.log(desChartData);
+
   const chartData = {
     labels: posDates,
     datasets: [
@@ -48,9 +63,39 @@ const ChartGraph = props => {
     ]
   };
 
+  const mortData = {
+    labels: posDates,
+    datasets: [
+      {
+        label: 'Mortality in Us',
+        data: desChartData,
+        backgroundColor: [
+          // 'rgba(255, 99, 132, 0.6)',
+          'rgba(54, 162, 235, 0.6)',
+          'rgba(255, 206, 86, 0.6)',
+          'rgba(75, 192, 192, 0.6)'
+        ],
+        borderWidth: 3
+      }
+    ],
+    options: {
+      scales: {
+        xAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+              // min: 0
+            }
+          }
+        ]
+      }
+    }
+  };
+
   return (
     <div className="charts-container">
       <Line data={chartData} />
+      <Line data={mortData} />
       {/* <Line /> */}
     </div>
   );
